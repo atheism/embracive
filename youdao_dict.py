cd2fcb55b8aa8de33ccde86dfbfb9f0bfd3a15e7
Atheism Zhang <zxqmailtoyou@gmail.com>
2013-01-07 09:53:41 +0800

    add sopcast comandline player with sop.list.

--- youdao_dict.py
+++ youdao_dict.py
@@ -4,7 +4,7 @@
 import sys
 import json
 import urllib2
-world = sys.argv[1]
+world = sys.argv[1:]
 #data = '{"type":"EN2ZH_CN","errorCode":0,"elapsedTime":79,"translateResult":[[{"src":"stale","tgt":"陈腐"}]],"smartResult":{"type":1,"entries":["","n. 尿","adj. 陈腐的；不新鲜的","vt. 使变旧；变得不新鲜"]}}'
 data = urllib2.urlopen("http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=dict.top?type=AUTO&i=%s&doctype=json&xmlVersion=1.4&keyfrom=fanyi.web&ue=UTF-8&typoResult=true&flag=false" % world)
 
